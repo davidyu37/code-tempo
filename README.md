82890003cdd3e30f4aeea93eac366c1f87e4f1ff
David Yu <davidyu37@aliyun.com>
2022-05-18 21:39:04 +0800

    playing with spotify api

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# muscial-key README
+# code-tempo README
 
-This is the README for your extension "muscial-key". After writing up a brief description, we recommend including the following sections.
+This is the README for your extension "code-tempo". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,39 @@
-import { commands, window, ExtensionContext } from 'vscode';
+import 'dotenv/config';
+import { ExtensionContext } from 'vscode';
 import { createCommands } from './commands';
+import * as CodyMusic from "cody-music";
 
 export function activate(context: ExtensionContext) {
 	console.log('Musical Keys is ON');
+
+	const config: CodyMusic.CodyConfig = {
+		spotifyAccessToken: '',
+		spotifyRefreshToken: '',
+		spotifyClientSecret: process.env.SPOTIFY_CLIENT_ID || '',
+		spotifyClientId: process.env.SPOTIFY_CLIENT_SECRET || '',
+		enableItunesDesktop: false,
+		enableSpotifyDesktop: true,
+		enableSpotifyApi: true,
+		enableItunesDesktopSongTracking: false,
+	};
+
+	CodyMusic.setConfig(config);
+
+	// CodyMusic.playSpotifyPlaylist(
+	// 	CodyMusic.PlayerName.SpotifyDesktop,
+	// 	'7dAAuI50JwRIrm1sq6hhDE'
+	// ).then(res => {
+	// 	console.log(res);
+	// });
+
+	CodyMusic.playTrack(
+		CodyMusic.PlayerName.SpotifyDesktop,
+		"spotify:track:2YarjDYjBJuH63dUIh9OWv"
+	).then(result => {
+		console.log('playTrack', result);
+		// track is playing
+	});
+
 	
 	context.subscriptions.push(createCommands());
 }
